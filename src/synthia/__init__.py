cd6db81ced629b43e2557563690d7d051a820549
wjr21 <wjr21@sussex.ac.uk>
2026-09-03 11:35:25 +0100

    Don't hardcode the version number

--- src/synthia/__init__.py
+++ src/synthia/__init__.py
@@ -1,3 +1,12 @@
 """Agent tools for the Synthesizer Python package."""
 
-__version__ = "0.1.0.dev0"
+try:
+    # Written at build time from the git tag, as in Synthesizer.
+    from synthia._version import __version__
+except ImportError:
+    # Importable from an unbuilt checkout; there is no tag to report.
+    __version__ = "0.0.0.dev0+source"
+
+__all__ = [
+    "__version__",
+]
